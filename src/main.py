c0eb66cdb7d8cb691bb76e06c4b508040a837d83
Naren Sengodan <scartegy@gmail.com>
2024-06-17 20:24:32 +0530

    Added more flexibilty in dataset importing and input measuring

--- src/main.py
+++ src/main.py
@@ -1,10 +1,31 @@
 import torch
+import tensorflow_datasets as tfds
 from CNN import create_cnn
 
 def import_dataset():
-    pass
+
+    dataset_option = input("Which dataset do you want to import\n(Note: Tensorflow Datasets only) ")
+    dataset, info = tfds.load(dataset_option, with_info=True, as_supervised=True)
+
+    x_train, y_train = tfds.as_numpy(dataset['train'])
+    x_test, y_test = tfds.as_numpy(dataset['test'])
+
+    x_train_tensor = torch.tensor(x_train)
+    y_train_tensor = torch.tensor(y_train)
+    x_test_tensor = torch.tensor(x_test)
+    y_test_tensor = torch.tensor(y_test)
+
+    input_prompt = input("Do you want to obtain input size from dataset? (y/n): ")
+    if input_prompt.lower() == "y":
+        input_size = x_train[0]
+    else:
+        None
+
+    return x_train_tensor, y_train_tensor, x_test_tensor, y_test_tensor, input_size
 
 def main():
+
+    x_train_tensor, y_train_tensor, x_test_tensor, y_test_tensor, input_size = import_dataset()
 
     layer_type = input("Enter layer type: ")
 
@@ -13,10 +34,15 @@ def main():
         print(model)
         return model
 
-def CNN_main():
+def CNN_main(input_size):
 
     model = torch.nn.Sequential()
-    input_size = int(input("Enter input size: "))
+    
+    if input_size:
+        pass
+    else:
+        input_size = int(input("Enter input size: "))
+
     num_layers_cnn = int(input("Enter number of layers in CNN: "))
     in_chan = int(input("Enter number of input channels: "))
     out_chan = int(input("Enter number of output channels: "))
