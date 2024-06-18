6fe372a62e25a749e722c760142518ac5398e2a2
Naren Sengodan <scartegy@gmail.com>
2024-06-18 15:21:46 +0530

    Fixed minor bugs

--- builder/main.py
+++ builder/main.py
@@ -88,26 +88,28 @@ def main():
 
     x_train, x_test = import_dataset()
     print("Welcome to Hephaestus ")
-    layer_type = input("Enter layer type:\n")
+    
     train_prompt = input("Do you want to train the model? (y/n): ")
     save_prompt = input("Do you want to save the model? (y/n): ")
 
-    if layer_type.lower() == "cnn":
-        model = CNN_main(x_train, x_test)
-        print(model)
-        
-        if train_prompt.lower() == "y":
-            train_model(model,x_train,x_test)
-            print("Model trained")
-        else:
-            print("Model not trained")
     
-        if save_prompt.lower() == "y":
-            model_save()
-            print("Model saved")
-        else:
-            print("Model not saved")
-            pass
+    model = CNN_main(x_train, x_test)
+    print(model)
+
+    if save_prompt.lower() == "y":
+        model_save()
+        print("Model saved")
+    else:
+        print("Model not saved")
+        pass
+        
+    if train_prompt.lower() == "y":
+        train_model(model,x_train,x_test)
+        print("Model trained")
+    else:
+        print("Model not trained")
+    
+
     
 if __name__ == "__main__":
     model = main()
